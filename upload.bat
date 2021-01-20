5457abd52b1551340a06eb7e7533f56f465c324c
sikbrad <sikbrad@gmail.com>
2021-01-21 03:07:22 +0900

    commit

--- upload.bat
+++ upload.bat
@@ -1,7 +1,9 @@
 git add .
 git commit -m "commit"
 git push
-:: open build status
-start https://github.com/sikbrad/sikbrad.github.io/actions
+
 :: open page
 start https://sikbrad.github.io/
+
+:: open build status
+start https://github.com/sikbrad/sikbrad.github.io/actions
